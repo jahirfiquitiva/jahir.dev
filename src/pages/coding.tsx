c651685cdb9bc1cea8ec68005d7f9816d6436dbc
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2021-09-27 17:31:05 -0500

    Add coding site

---
+++ src/pages/coding.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react';
+
+import { Error } from '~/blocks/error';
+import { Page } from '~/blocks/page';
+
+const FourHundredFour: FC = () => {
+  return (
+    <Page title={'Uses ~ Jahir Fiquitiva 💎'}>
+      <Error errorType={'under-construction'} />
+    </Page>
+  );
+};
+
+export default FourHundredFour;
