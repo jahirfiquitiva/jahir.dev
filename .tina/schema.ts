d824de09040301644a7d32f3a44ca0764ba260fd
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2021-11-02 20:39:20 -0500

    Remove .tina folder

--- .tina/schema.ts
+++
@@ -1,8 +0,0 @@
-import { defineSchema } from '@tinacms/cli';
-
-// import { challengesSchema } from './challenges-schema';
-import { postsSchema } from './posts-schema';
-
-export default defineSchema({
-  collections: [postsSchema], // , challengesSchema],
-});
