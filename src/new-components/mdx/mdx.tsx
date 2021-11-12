12a7128a759b728a73646b5de74eb2d82fc98a09
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2021-11-12 02:06:52 -0500

    Revert last commit

--- src/new-components/mdx/mdx.tsx
+++ src/new-components/mdx/mdx.tsx
@@ -3,7 +3,7 @@ import { ImageComparison } from './image-comparison';
 import { Image, ImageProps, Link } from '~/new-components/atoms/simple';
 
 export const mdxComponents = {
-  // img: (props: ImageProps) => <Image {...props} avoidNextImage />,
+  img: (props: ImageProps) => <Image {...props} avoidNextImage />,
   Image,
   ImageComparison,
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
