b164a79114be8cbcb1975e9f060fcdb4081b16d0
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2021-11-12 19:33:14 -0500

    Fix theme provider to allow components rendering

--- src/providers/theme.tsx
+++ src/providers/theme.tsx
@@ -1,28 +1,29 @@
 import { useTheme as useNextTheme } from 'next-themes';
 import { createContext, useContext, useState, useEffect } from 'react';
 
-import { Component, ComponentProps } from '~/elements/base/fc';
+import { Component } from '~/elements/base/fc';
 
 export interface ThemeContextValue {
-  isDark?: boolean;
+  isDark: boolean;
+  themeReady: boolean;
   toggleTheme?: () => void;
 }
 
-export interface ThemeProps extends ThemeContextValue, ComponentProps {}
+const ThemeContext = createContext<ThemeContextValue>({
+  isDark: false,
+  themeReady: false,
+});
 
-const ThemeContext = createContext<ThemeContextValue>({ isDark: false });
-
-export const ThemeProvider: Component<ThemeProps> = (props) => {
-  const [mounted, setMounted] = useState(false);
+export const ThemeProvider: Component = (props) => {
   const { theme, resolvedTheme, setTheme } = useNextTheme();
 
+  const [mounted, setMounted] = useState(false);
   useEffect(() => setMounted(true), []);
-
-  if (!mounted) return null;
 
   const { children } = props;
 
   const themeContextValue: ThemeContextValue = {
+    themeReady: mounted,
     isDark: (resolvedTheme || theme) === 'dark',
     toggleTheme: () => {
       setTheme(resolvedTheme === 'dark' ? 'light' : 'dark');
@@ -38,13 +39,8 @@ export const ThemeProvider: Component<ThemeProps> = (props) => {
 
 export const useTheme = (): ThemeContextValue => {
   const themeState = useContext(ThemeContext);
-
   if (themeState === undefined) {
     throw new Error('useTheme must be used within a ThemeProvider');
   }
-
-  return {
-    isDark: themeState.isDark || false,
-    toggleTheme: themeState.toggleTheme,
-  };
+  return themeState;
 };
