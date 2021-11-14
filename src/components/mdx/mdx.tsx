dbaaab4e9a1a46f22ccdcc7427620478dad37b34
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2021-11-14 01:00:06 -0500

    Update uses page and create simple grid component for blog posts

--- src/components/mdx/mdx.tsx
+++ src/components/mdx/mdx.tsx
@@ -1,11 +1,14 @@
 import { ImageComparison } from './image-comparison';
 import { MdxLink } from './mdx-link';
+import { SimpleGrid, SimpleGridColumn } from './simple-grid';
 
 import { Image, ImageProps } from '~/components/atoms/simple';
 
 export const mdxComponents = {
+  a: MdxLink,
   img: (props: ImageProps) => <Image {...props} avoidNextImage />,
   Image,
   ImageComparison,
-  a: MdxLink,
+  SimpleGrid,
+  SimpleGridColumn,
 };
