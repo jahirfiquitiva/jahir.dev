28cec1ffc8e3568cb84ea12efd5b91540a7e82ea
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2021-11-14 00:40:19 -0500

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -77,8 +77,6 @@ module.exports = {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
     require('tailwindcss-textshadow'),
   ],
 };
