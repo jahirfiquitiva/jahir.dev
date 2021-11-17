5575196b0599a622facb14dc4762c03418363050
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2021-11-17 00:05:50 -0500

    Update generated rss

--- scripts/generate-rss.mjs
+++ scripts/generate-rss.mjs
@@ -106,9 +106,9 @@ const buildFeed = (posts) => {
     rss: [
       {
         _attr: {
-          'xmlns:dc': 'http://purl.org/dc/elements/1.1/',
-          'xmlns:content': 'http://purl.org/rss/1.0/modules/content/',
-          'xmlns:atom': 'http://www.w3.org/2005/Atom',
+          // 'xmlns:dc': 'http://purl.org/dc/elements/1.1/',
+          // 'xmlns:content': 'http://purl.org/rss/1.0/modules/content/',
+          // 'xmlns:atom': 'http://www.w3.org/2005/Atom',
           version: '2.0',
         },
       },
