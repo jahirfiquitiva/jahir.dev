b0c5da9874257f4bdbcea8f124d5933069530ddd
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2021-11-16 23:03:03 -0500

    Implement new libs to generate rss feed

--- scripts/generate-rss.mjs
+++ scripts/generate-rss.mjs
@@ -1,11 +1,14 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { writeFileSync } from 'fs';
 
+import cheerio from 'cheerio';
 import { escape } from 'html-escaper';
+import js2xml from 'js2xmlparser';
 import remarkHtml from 'remark-html';
 import remarkParse from 'remark-parse';
 import RSS from 'rss';
 import { unified } from 'unified';
+import xml from 'xml';
 
 import { allBlogs } from '.contentlayer/data';
 
@@ -23,25 +26,102 @@ const getAllPostRssData = async (post) => {
     title: post.title,
     url: post.link || `https://jahir.dev/blog/${post.slug}`,
     date: post.date,
-    description: escape(html) || post.excerpt,
+    description: post.excerpt,
+    html: escape(html),
+    slug: post.slug,
   };
 };
 
+const buildFeed = (posts) => {
+  const sortedPosts = posts.sort(function (first, second) {
+    return new Date(second.date).getTime() - new Date(first.date).getTime();
+  });
+  const feedItems = [];
+
+  feedItems.push(
+    ...sortedPosts.map(function (post) {
+      const description = post.html ? post.html : { _cdata: post.description };
+      const feedItem = {
+        item: [
+          { title: post.title },
+          {
+            pubDate: new Date(post.date).toUTCString(),
+          },
+          { url: `https://jahir.dev/blog/${post.slug}` },
+          {
+            guid: [
+              { _attr: { isPermaLink: true } },
+              `${post.slug}-${new Date().getTime()}`,
+            ],
+          },
+          {
+            description,
+          },
+        ],
+      };
+
+      return feedItem;
+    }),
+  );
+
+  return feedItems;
+};
+
+// <rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
+
 (async () => {
-  const feed = new RSS({
+  const baseRss = {
     title: 'Jahir Fiquitiva',
     description:
       'Passionate and creative full-stack developer from Colombia &#x1f1e8;&#x1f1f4;',
     site_url: 'https://jahir.dev',
     feed_url: 'https://jahir.dev/feed.xml',
     language: 'en',
-  });
-
+  };
   const feedItems = await Promise.all(allBlogs.map(getAllPostRssData));
 
-  feedItems.forEach((post) => {
-    feed.item(post);
-  });
+  const feedObject = {
+    rss: [
+      {
+        _attr: {
+          'xmlns:dc': 'http://purl.org/dc/elements/1.1/',
+          'xmlns:content': 'http://purl.org/rss/1.0/modules/content/',
+          'xmlns:atom': 'http://www.w3.org/2005/Atom',
+          version: '2.0',
+        },
+      },
+      {
+        channel: [
+          {
+            'atom:link': {
+              _attr: {
+                href: 'https://jahir.dev/feed.xml',
+                rel: 'self',
+                type: 'application/rss+xml',
+              },
+            },
+          },
+          { image_url: 'https://jahir.dev/static/images/brand/logo-def.png' },
+          {
+            title: 'Jahir Fiquitiva',
+          },
+          {
+            link: 'https://jahir.dev',
+          },
+          {
+            description:
+              'Passionate and creative full-stack developer from Colombia &#x1f1e8;&#x1f1f4;',
+          },
+          { language: 'en-US' },
+          { lastBuildDate: new Date().toUTCString() },
+          ...buildFeed(feedItems),
+        ],
+      },
+    ],
+  };
 
-  writeFileSync('./public/feed.xml', feed.xml({ indent: true }));
+  const feed = `<?xml version="1.0" encoding="UTF-8"?>\n${xml(feedObject, {
+    indent: '  ',
+  })}`;
+  writeFileSync('./public/feed.xml', feed);
 })();
