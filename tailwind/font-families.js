fc93511f0fe5086a62fa9a5c1c581270338cfbb3
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2021-11-21 00:37:04 -0500

    Remove tailwind and twin.macro

--- tailwind/font-families.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const { fontFamily } = require('tailwindcss/defaultTheme');
-
-module.exports = {
-  sans: ['Inter', ...fontFamily.sans],
-  manrope: ['Manrope', 'Inter', ...fontFamily.sans],
-  mono: ['Fira Code', ...fontFamily.mono],
-};
