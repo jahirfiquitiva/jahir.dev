7b947691aef810e1782bcb52a62d3335f7e4f6c6
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2021-12-11 19:43:33 -0500

    Update dependencies. Improve cross platform compatibility

--- next.config.js
+++ next.config.js
@@ -37,6 +37,7 @@ const defaultNextConfig = {
   webpack(config, { dev, isServer }) {
     if (!dev && !isServer) {
       Object.assign(config.resolve.alias, {
+        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
         react: 'preact/compat',
         'react-dom': 'preact/compat',
         'react-dom/test-utils': 'preact/test-utils',
