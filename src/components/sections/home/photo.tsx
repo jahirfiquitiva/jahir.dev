04eea5679c6dab91305fa5bc259556eb77c2b9ac
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2022-03-14 19:59:12 -0500

    Update photo styles

--- src/components/sections/home/photo.tsx
+++ src/components/sections/home/photo.tsx
@@ -8,7 +8,6 @@ const PhotoContainer = styled.div`
   justify-content: flex-start;
   margin-bottom: 0.8rem;
   grid-row: 1;
-  filter: drop-shadow(0 4px 3px rgba(var(--shadow-color), 0.12));
 
   ${mediaQueries.tablet.sm} {
     justify-content: flex-end;
@@ -19,7 +18,13 @@ const PhotoContainer = styled.div`
 const PhotoImage = styled(Image)`
   border-radius: 50%;
   padding: 0.4rem !important;
-  filter: drop-shadow(0 1px 2px rgba(var(--shadow-color), 0.04));
+  transition: all 0.25s ease-in-out;
+  filter: opacity(0.9) brightness(1.1);
+
+  &:hover,
+  &:focus {
+    filter: opacity(1) brightness(1.1);
+  }
 `;
 
 export const Photo: Component = (props) => {
@@ -29,7 +34,7 @@ export const Photo: Component = (props) => {
       <PhotoImage
         src={'/static/images/jahir/jahir-hd.jpg'}
         alt={"Jahir's Photo"}
-        size={168}
+        size={180}
         priority
       />
     </PhotoContainer>
