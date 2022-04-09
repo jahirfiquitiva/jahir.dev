64ea9a39d344eacbf90bd8440830c50412259750
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2022-04-09 16:53:10 -0500

    Clean up dashboard intro component

--- src/components/sections/dashboard/now.tsx
+++ src/components/sections/dashboard/now.tsx
@@ -3,27 +3,22 @@ import styled from '@emotion/styled';
 import { Heading, Image } from '~/components/atoms/simple';
 import { Component, mediaQueries } from '~/types';
 
-const NowHeading = styled(Heading)`
-  margin-top: 1.2rem;
-  ${mediaQueries.tablet.sm} {
-    margin-top: 0;
-  }
-`;
-
 const NowContainer = styled.div`
-  display: grid;
-  grid-template-columns: minmax(0, 1fr);
+  display: flex;
+  flex-direction: column;
   margin-bottom: 1.2rem;
 
   ${mediaQueries.tablet.sm} {
-    gap: 2.4rem;
+    gap: 1.6rem;
     align-items: center;
   }
 `;
 
-const AnimojiContainer = styled.div`
+const NowHeading = styled(Heading)`
+  align-self: flex-start;
+  margin-top: 1.2rem;
   ${mediaQueries.tablet.sm} {
-    margin: 0 auto;
+    margin-top: 0;
   }
 `;
 
@@ -36,15 +31,13 @@ export const Now: Component = () => {
   return (
     <>
       <NowContainer>
-        <AnimojiContainer>
-          <Animoji
-            src={'/static/images/jahir/animoji.png'}
-            alt={'Jahir as an Animoji'}
-            size={144}
-            quality={100}
-            priority
-          />
-        </AnimojiContainer>
+        <Animoji
+          src={'/static/images/jahir/animoji.png'}
+          alt={'Jahir as an Animoji'}
+          size={144}
+          quality={100}
+          priority
+        />
         <NowHeading
           size={'3'}
           shadowColor={'purple'}
