7c8fee0acb584ff275f8ed56e747d0099b618d6b
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2022-07-14 13:45:05 -0500

    Disable spotlight

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import { ThemeProvider as NextThemeProvider } from 'next-themes';
 import { AppProps } from 'next/app';
 
 import { ThemeProvider } from '@/providers/theme';
-import { SpotlightProvider } from '@/providers/spotlight';
+// import { SpotlightProvider } from '@/providers/spotlight';
 import type { FC } from '@/types';
 import { darkTheme, globalStyles } from '~/stitches';
 import '@/styles/globals.scss';
@@ -21,9 +21,9 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
       disableTransitionOnChange
     >
       <ThemeProvider>
-        <SpotlightProvider>
-          <Component {...pageProps} />
-        </SpotlightProvider>
+        {/* <SpotlightProvider> */}
+        <Component {...pageProps} />
+        {/* </SpotlightProvider> */}
       </ThemeProvider>
     </NextThemeProvider>
   );
