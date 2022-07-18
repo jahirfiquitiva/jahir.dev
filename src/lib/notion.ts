dd73b07e5fb096caea9fcb8a791825704ab64f37
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2022-07-18 14:07:45 -0500

    Prepare API to get Bookmarks from Notion

---
+++ src/lib/notion.ts
@@ -0,0 +1,151 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable camelcase */
+const dbApiUrl = 'https://api.notion.com/v1';
+const {
+  NOTION_INTEGRATION_TOKEN: notionToken = '',
+  NOTION_BOOKMARKS_DB_ID: bookmarksDbId = '',
+} = process.env;
+const NOTION_API_VERSION = '2022-06-28';
+const authHeaders =
+  notionToken && notionToken.length > 0
+    ? {
+        Authorization: `Bearer ${notionToken}`,
+        'Notion-Version': NOTION_API_VERSION,
+        'Content-Type': 'application/json',
+      }
+    : {};
+
+const hiddenCategories = ['Tools', 'Blog Posts', 'Private'];
+
+const buildAndFilter = () => {
+  const filters = [];
+  for (const category of hiddenCategories) {
+    filters.push({
+      property: 'Type',
+      multi_select: {
+        does_not_contain: category,
+      },
+    });
+  }
+  return filters;
+};
+
+const raw = JSON.stringify({
+  filter: {
+    and: buildAndFilter(),
+  },
+  sorts: [
+    {
+      property: 'Title',
+      timestamp: 'created_time',
+      direction: 'ascending',
+    },
+  ],
+});
+
+export const requestOptions = {
+  method: 'POST',
+  headers: authHeaders,
+  body: raw,
+};
+
+export const databaseQueryEndpoint = `${dbApiUrl}/databases/${bookmarksDbId}/query`;
+
+const fetchSinglePageProperty = (pageId: string, propertyId: string) => {
+  const endpoint = `${dbApiUrl}/pages/${pageId}/properties/${propertyId}`;
+  return fetch(endpoint, {
+    method: 'GET',
+    // @ts-ignore
+    headers: { ...authHeaders, Accept: 'application/json' },
+  }).then((response) => response.json());
+};
+
+const fetchPageProperties = (
+  pageId: string,
+  propertiesIds: Array<string>,
+) =>
+  propertiesIds.map((propertyId) =>
+    fetchSinglePageProperty(pageId, propertyId),
+  );
+
+interface ExternalIconProperty {
+  type: 'external';
+  external: {
+    url: string;
+  };
+}
+
+interface FileIconProperty {
+  type: 'file';
+  file: {
+    url: string;
+    expiry_time: `${number}-${number}-${number}T${number}:${number}:${number}.${number}Z`;
+  };
+}
+
+interface EmojiIconProperty {
+  type: 'emoji';
+  emoji: string;
+}
+
+type IconProperty = ExternalIconProperty | FileIconProperty | EmojiIconProperty;
+
+interface PageProperty {
+  id: string;
+}
+
+interface BookmarkApiResult {
+  id: string;
+  icon: IconProperty;
+  properties: {
+    Title: PageProperty;
+    Link: PageProperty;
+    Type: PageProperty;
+  };
+}
+
+export const fetchBookmarkPromise = (result: BookmarkApiResult) => {
+  // eslint-disable-next-line no-async-promise-executor
+  return new Promise(async (resolve) => {
+    const { id, icon, properties } = result;
+    const { Title, Link, Type } = properties;
+
+    const propertiesResponse = await Promise.all(
+      fetchPageProperties(id, [Title.id, Link.id, Type.id]),
+    ).catch(() => []);
+
+    if (!propertiesResponse || !propertiesResponse.length) return null;
+
+    const [typeObject] = (propertiesResponse || []).filter(
+      (it) => it.id == Type.id,
+    );
+    const isHidden = (typeObject['multi_select'] || [])
+      // @ts-ignore
+      .some((type) => {
+        return hiddenCategories.includes(type.name);
+      });
+    if (isHidden) return null;
+
+    const [titleObject] =
+      propertiesResponse.filter(
+        (it) => it.object == 'list' || it.id == Title.id,
+      )?.[0]?.results || [];
+    const [urlObject] = propertiesResponse.filter((it) => it.id == Link.id);
+
+    const title = titleObject?.title?.text?.content;
+    // @ts-ignore
+    const iconUrl = icon?.url;
+    const emoji = (icon as EmojiIconProperty)?.emoji;
+    const domain = (urlObject?.url || '')
+      .replace(/(^\w+:|^)\/\//, '')
+      .replace(/\//g, '');
+
+    return resolve({
+      link: urlObject?.url,
+      title,
+      icon: iconUrl,
+      domain,
+      emoji,
+    });
+  });
+};
---
+++ src/pages/api/bookmarks.ts
@@ -0,0 +1,41 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable camelcase */
+import {
+  databaseQueryEndpoint,
+  requestOptions,
+  fetchBookmarkPromise,
+} from '@/lib/notion';
+import { buildApiResponse } from '@/utils';
+
+export const config = {
+  runtime: 'experimental-edge',
+};
+
+export default async function handler() {
+  try {
+    const databaseRequest = await fetch(
+      databaseQueryEndpoint,
+      // @ts-ignore
+      requestOptions,
+    );
+    const { results = [] } = await databaseRequest.json();
+    const bookmarksPromises = results.map(fetchBookmarkPromise);
+    return buildApiResponse(
+      200,
+      {
+        success: true,
+        bookmarks: (await Promise.all(bookmarksPromises)
+          .catch(() => []))
+          // @ts-ignore
+          .filter((it) => it),
+      },
+      { 'cache-control': 'public, s-maxage=3600, stale-while-revalidate=1800' },
+    );
+  } catch (err) {
+    return buildApiResponse(400, {
+      success: false,
+      // @ts-ignore
+      error: err?.message || err?.stackTrace.toString() || 'Unexpected error',
+    });
+  }
+}
