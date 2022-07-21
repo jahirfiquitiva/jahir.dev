48103ae40d7c1a4824e34d7f87eab7e6c76507f4
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2022-07-21 14:09:57 -0500

    Update testimonials

--- src/lib/sponsors/testimonials.ts
+++ src/lib/sponsors/testimonials.ts
@@ -9,4 +9,18 @@ export const testimonials: Array<Testimonial> = [
       name: 'Patryk Michalik',
     },
   },
+  {
+    content:
+      'Supporting Jahir was easy, not only because he has earned it with some of his great apps, but because of the selfless way he goes about assisting communities with his work.',
+    sponsor: {
+      name: 'Eduardo Pratti',
+    },
+  },
+  {
+    content:
+      "I've been using Jahir’s dashboards to create and publish five icon packs before I seriously got into Android apps development.",
+    sponsor: {
+      name: 'Patryk Goworowski',
+    },
+  },
 ];
