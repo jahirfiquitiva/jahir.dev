161f39ec52f9957cacff3ece45e1767e13f99d74
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2022-07-21 17:47:22 -0500

    Remove getServerSideProps for Inspiration

--- src/pages/inspiration.tsx
+++ src/pages/inspiration.tsx
@@ -1,54 +1,18 @@
-import type { GetServerSideProps, NextPage } from 'next';
+import type { NextPage } from 'next';
 import Head from 'next/head';
-import { SWRConfig } from 'swr';
 
 import { Layout } from '@/components/elements';
-import fetcher from '@/lib/fetcher';
-import { InspirationItem } from '@/lib/notion';
 import { Inspiration as InspirationSection } from '@/sections';
 
-interface BookmarksResponse {
-  success?: boolean;
-  bookmarks?: Array<InspirationItem>;
-}
-
-interface InspirationProps {
-  fallback?: BookmarksResponse;
-}
-
-const Inspiration: NextPage<InspirationProps> = ({ fallback }) => {
+const Inspiration: NextPage = () => {
   return (
     <Layout>
       <Head>
         <title>Inspiration | Jahir Fiquitiva</title>
       </Head>
-      <SWRConfig value={{ fallback }}>
-        <InspirationSection />
-      </SWRConfig>
+      <InspirationSection />
     </Layout>
   );
 };
 
 export default Inspiration;
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  try {
-    const bookmarks: BookmarksResponse = await fetcher(
-      'https://jahir.dev/api/bookmarks',
-    );
-    return {
-      props: {
-        fallback: {
-          '/api/bookmarks': {
-            success: bookmarks.success,
-            bookmarks: bookmarks.bookmarks?.filter((it) => !!it && !!it.link),
-          },
-        },
-      },
-    };
-  } catch (e) {
-    return {
-      props: {},
-    };
-  }
-};
