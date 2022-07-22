ecacc5779303b505b4084479cb20885be26396ad
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2022-07-22 15:07:06 -0500

    Add projects to sitemap

--- scripts/generate-sitemap.mjs
+++ scripts/generate-sitemap.mjs
@@ -5,7 +5,7 @@ import { writeFileSync } from 'fs';
 import { globby } from 'globby';
 import prettier from 'prettier';
 
-import { allBlogs } from './../.contentlayer/generated/index.mjs';
+import { allBlogs, allProjects } from './../.contentlayer/generated/index.mjs';
 
 (async () => {
   const prettierConfig = await prettier.resolveConfig('./.prettierrc.js');
@@ -47,9 +47,15 @@ import { allBlogs } from './../.contentlayer/generated/index.mjs';
       .map((it) => `/blog/${it.slug}`),
   );
 
+  const projectsRoutes = await Promise.all(
+    allProjects
+      .filter((it) => !(it.inProgress || it.hide))
+      .map((it) => `/project/${it.slug}`),
+  );
+
   const now = new Date().toISOString();
   const lastmod = `${now.substring(0, now.lastIndexOf('.'))}+00:00`;
-  const xmlRoutes = [...actualRoutes, ...blogPostsRoutes]
+  const xmlRoutes = [...actualRoutes, ...blogPostsRoutes, ...projectsRoutes]
     .map((route) => {
       const slashesCount = (route.match(/\//g) || []).length;
       let priority = 1 - 0.2 * slashesCount;
