078f708a6f888e519962e98e542a322f496c54e0
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2022-07-22 17:36:48 -0500

    Fix export

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,6 @@ export * from './color/get-color-from-palette';
 export * from './color/get-readable-color';
 export * from './color/hex-to-rgb';
 export * from './format/format-date';
-export * from './icon-paths';
 export * from './posts/get-posts';
 export * from './posts/group-posts';
 export * from './response';
