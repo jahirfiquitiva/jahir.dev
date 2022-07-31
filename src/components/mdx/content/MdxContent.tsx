08ef08fe878e240987a973eba77d6a1ffa022b84
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2022-07-31 17:13:44 -0500

    Remove unused import

--- src/components/mdx/content/MdxContent.tsx
+++ src/components/mdx/content/MdxContent.tsx
@@ -8,12 +8,7 @@ import { ReactionsProvider } from '@/providers/reactions';
 import { useTheme } from '@/providers/theme';
 import type { FC, HeroMeta, Post, Project } from '@/types';
 import { getDomainFromUrl } from '@/utils';
-import {
-  formatDate,
-  hexToRGB,
-  getColorFromPalette,
-  getReadableColor,
-} from '@/utils';
+import { formatDate, hexToRGB, getReadableColor } from '@/utils';
 import type { StitchesCSS as CSS } from '~/stitches';
 
 import { Article } from './Article';
