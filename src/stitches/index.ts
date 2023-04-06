ca0f6817a79506a504720b05a195694ecb569d9d
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2023-04-05 20:17:21 -0500

    refactor: rename fonts files

--- src/stitches/index.ts
+++ src/stitches/index.ts
@@ -3,8 +3,8 @@ export * from './colors';
 export * from './font-sizes';
 export * from './spaces';
 export * from './utils';
-import * as Inter from './fonts/inter-old';
-import * as Manrope from './fonts/manrope-old';
+import * as Inter from './fonts/inter';
+import * as Manrope from './fonts/manrope';
 export const fonts = {
   Inter,
   Manrope,
