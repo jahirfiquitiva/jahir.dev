931cceac51dde0944704eff0cee7fe77abfd4315
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2023-04-05 20:34:33 -0500

    feat: updates views api with kysely

--- src/pages/api/views/index.ts
+++ src/pages/api/views/index.ts
@@ -1,24 +1,22 @@
-/* eslint-disable no-undef */
-/* eslint-disable @typescript-eslint/ban-ts-comment */
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 import { getDevToArticles } from '@/lib/devto';
-import prisma from '@/lib/prisma';
+import { queryBuilder } from '@/lib/planetscale';
 
-// @ts-ignore
-BigInt.prototype.toJSON = function () {
-  return this.toString();
-};
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
   try {
-    const totalViews = await prisma.counters.aggregate({
-      _sum: {
-        views: true,
-      },
-    });
+    const data = await queryBuilder
+      .selectFrom('counters')
+      .select(['slug', 'views'])
+      .execute();
+
+    const views: number = data.reduce(
+      (acc, item) => acc + Number(item.views || 0),
+      0,
+    );
 
     let devToViews = 0;
     const devArticlesRequest = await getDevToArticles();
@@ -28,7 +26,6 @@ export default async function handler(
       devToViews = devArticles
         .filter((it: { published?: boolean }) => it.published)
         .reduce(
-          // eslint-disable-next-line
           (accumulator: number, article: { page_views_count: number }) => {
             const { page_views_count: views = 0 } = article;
             return accumulator + views;
@@ -39,14 +36,12 @@ export default async function handler(
 
     return res.status(200).send({
       success: true,
-      total: (
-        // eslint-disable-next-line no-underscore-dangle
-        (totalViews?._sum?.views || BigInt(0)) + BigInt(devToViews)
-      ).toString(),
+      total: (views + devToViews).toString(),
     });
   } catch (err) {
     return res.status(500).send({
       success: false,
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
       error: err?.message || err?.stackTrace.toString() || 'Unexpected error',
     });
