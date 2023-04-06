4bcd305ecfa213ef963ee7554c9192379820604d
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2023-04-06 12:16:16 -0500

    feat: finish og api

--- src/pages/api/og.tsx
+++ src/pages/api/og.tsx
@@ -7,12 +7,118 @@ import { NextRequest } from 'next/server';
 export const runtime = 'edge';
 
 const font = fetch(
-  new URL('../../assets/fonts/manrope-variable.ttf', import.meta.url),
+  new URL('../../assets/fonts/manrope-bold.ttf', import.meta.url),
 ).then((res) => res.arrayBuffer());
+
+const pathEmojiMap = {
+  '404': '👻',
+  about: '😀',
+  dashboard: 'ℹ️',
+  donate: '🎁',
+  projects: '💼',
+  uses: '💻',
+  blog: '📄',
+};
+type PathName = keyof typeof pathEmojiMap | null;
+
+const LogoOrEmoji = (props: { path?: PathName }) => {
+  const emoji = props.path ? pathEmojiMap[props.path] : null;
+  if (!emoji) {
+    return (
+      <svg
+        className={'logo'}
+        viewBox={'0 0 32 32'}
+        xmlns={'http://www.w3.org/2000/svg'}
+        style={{
+          filter: 'drop-shadow(0 1px 2px rgba(8, 15, 30, 0.5))',
+          width: 96,
+          height: 96,
+        }}
+      >
+        <path
+          d={
+            'M25.4 3.9C22.8 1.3 19.4 0 16 0S9.2 1.3 6.6 3.9C1.5 9 1.5 17.5 6.6 22.6L16 32l9.4-9.4c5.1-5.1 5.1-13.6 0-18.7zM10 9.1V8h5.8c-.7.5-1.3 1.3-1.4 2.2h-3.3c-.6 0-1.1-.5-1.1-1.1zM21.5 14c0 .6-.5 1.1-1.1 1.1h-2.2c-.6 0-1.1.5-1.1 1.1v1.6c0 1.8-1.5 3.3-3.3 3.3s-3.3-1.5-3.3-3.3v-2.7h1.1c.6 0 1.1.5 1.1 1.1v1.6c0 .6.5 1.1 1.1 1.1s1.1-.5 1.1-1.1v-1.6c0-1.8 1.5-3.3 3.3-3.3h3.3zm.5-4.9c0 .6-.5 1.1-1.1 1.1h-2.7c-.6 0-1.1.5-1.1 1.1v1.1c-.9.2-1.7.7-2.2 1.4v-2.6c0-1.8 1.5-3.3 3.3-3.3H22z'
+          }
+          fill={'#88a4e6'}
+        />
+      </svg>
+    );
+  }
+  return <span style={{ fontSize: 56 }}>{emoji}</span>;
+};
+
+const PageTitle = (props: { path?: PathName; title?: string | null }) => {
+  const { path, title } = props;
+  return (
+    <p
+      style={{
+        fontSize: 56,
+        fontWeight: 700,
+        maxWidth: 900,
+        color: path ? 'white' : 'rgba(0, 0, 0, 0)',
+        ...(path
+          ? {}
+          : {
+              backgroundImage: 'linear-gradient(to right, #88a4e6, #81c1e9)',
+              backgroundClip: 'text',
+            }),
+      }}
+    >
+      {title || 'Jahir Fiquitiva'}
+    </p>
+  );
+};
+
+const Name = (props: { path?: PathName }) => {
+  if (!props.path) return null;
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        gap: 12,
+        fontSize: 32,
+      }}
+    >
+      <span>
+        <svg
+          className={'logo'}
+          viewBox={'0 0 32 32'}
+          xmlns={'http://www.w3.org/2000/svg'}
+          style={{ filter: 'drop-shadow(0 1px 2px rgba(8, 15, 30, 0.5))' }}
+        >
+          <path
+            d={
+              'M25.4 3.9C22.8 1.3 19.4 0 16 0S9.2 1.3 6.6 3.9C1.5 9 1.5 17.5 6.6 22.6L16 32l9.4-9.4c5.1-5.1 5.1-13.6 0-18.7zM10 9.1V8h5.8c-.7.5-1.3 1.3-1.4 2.2h-3.3c-.6 0-1.1-.5-1.1-1.1zM21.5 14c0 .6-.5 1.1-1.1 1.1h-2.2c-.6 0-1.1.5-1.1 1.1v1.6c0 1.8-1.5 3.3-3.3 3.3s-3.3-1.5-3.3-3.3v-2.7h1.1c.6 0 1.1.5 1.1 1.1v1.6c0 .6.5 1.1 1.1 1.1s1.1-.5 1.1-1.1v-1.6c0-1.8 1.5-3.3 3.3-3.3h3.3zm.5-4.9c0 .6-.5 1.1-1.1 1.1h-2.7c-.6 0-1.1.5-1.1 1.1v1.1c-.9.2-1.7.7-2.2 1.4v-2.6c0-1.8 1.5-3.3 3.3-3.3H22z'
+            }
+            fill={'#88a4e6'}
+          />
+        </svg>
+      </span>
+      <p
+        style={{
+          fontWeight: 700,
+          color: 'rgba(0, 0, 0, 0)',
+          backgroundImage: 'linear-gradient(to right, #88a4e6, #81c1e9)',
+          backgroundClip: 'text',
+        }}
+      >
+        Jahir Fiquitiva
+      </p>
+    </div>
+  );
+};
+
+const baseUrl =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000'
+    : 'https://jahir.dev';
 
 export default async function handler(req: NextRequest) {
   const { searchParams } = req.nextUrl;
-  const postTitle = searchParams.get('title');
+  const path = searchParams.get('path') as PathName;
+  const title = searchParams.get('title');
+  const postHero = searchParams.get('hero') || 'site/default-og.png';
   const fontData = await font;
 
   return new ImageResponse(
@@ -28,14 +134,15 @@ export default async function handler(req: NextRequest) {
           fontFamily: 'Manrope',
           fontSize: 28,
           color: 'white',
-          textShadow: '0 2px 4px rgba(12, 18, 30, 0.5)',
+          textShadow: '0 2px 4px rgba(8, 15, 30, 0.5)',
+          backgroundColor: 'rgb(8, 15, 30)',
         }}
       >
         <img
-          src={
-            'https://jahir.dev/static/images/blog/nextjs-api-github-sponsors/hero.png'
-          }
-          alt={postTitle || 'Hero image'}
+          src={`${baseUrl}/static/images/${postHero}`}
+          alt={title || 'Hero image'}
+          width={1200}
+          height={630}
           style={{
             position: 'absolute',
             width: 'auto',
@@ -51,47 +158,29 @@ export default async function handler(req: NextRequest) {
         />
         <div
           style={{
-            backgroundImage:
-              'linear-gradient(to right, rgba(12, 18, 30, 1) 0%, rgba(12, 18, 30, 0.5) 80%, rgba(12, 18, 30, 0) 100%)',
+            backgroundColor: 'rgba(8, 15, 30, 0.25)',
             position: 'absolute',
             top: 0,
             right: 0,
             left: 0,
             bottom: 0,
-            width: '100%',
           }}
         />
         <div
           style={{
-            display: 'flex',
-            alignItems: 'center',
-            gap: 12,
-            fontSize: 32,
+            backgroundImage:
+              'linear-gradient(68deg, rgba(8, 15, 30, 1) 0%, rgba(8, 15, 30, 0.5) 55%, rgba(8, 15, 30, 0) 100%)',
+            position: 'absolute',
+            top: 0,
+            right: 0,
+            left: 0,
+            bottom: 0,
           }}
-        >
-          <span>
-            <svg
-              className={'logo'}
-              viewBox={'0 0 32 32'}
-              xmlns={'http://www.w3.org/2000/svg'}
-              style={{ filter: 'drop-shadow(0 1px 2px rgba(12, 18, 30, 0.5))' }}
-            >
-              <path
-                d={
-                  'M25.4 3.9C22.8 1.3 19.4 0 16 0S9.2 1.3 6.6 3.9C1.5 9 1.5 17.5 6.6 22.6L16 32l9.4-9.4c5.1-5.1 5.1-13.6 0-18.7zM10 9.1V8h5.8c-.7.5-1.3 1.3-1.4 2.2h-3.3c-.6 0-1.1-.5-1.1-1.1zM21.5 14c0 .6-.5 1.1-1.1 1.1h-2.2c-.6 0-1.1.5-1.1 1.1v1.6c0 1.8-1.5 3.3-3.3 3.3s-3.3-1.5-3.3-3.3v-2.7h1.1c.6 0 1.1.5 1.1 1.1v1.6c0 .6.5 1.1 1.1 1.1s1.1-.5 1.1-1.1v-1.6c0-1.8 1.5-3.3 3.3-3.3h3.3zm.5-4.9c0 .6-.5 1.1-1.1 1.1h-2.7c-.6 0-1.1.5-1.1 1.1v1.1c-.9.2-1.7.7-2.2 1.4v-2.6c0-1.8 1.5-3.3 3.3-3.3H22z'
-                }
-                fill={'#88a4e6'}
-              />
-            </svg>
-          </span>
-          <span>
-            <strong>/</strong>
-          </span>
-          <span>📄</span>
-        </div>
-        <p style={{ fontSize: 48, fontWeight: 700, color: 'white' }}>
-          {postTitle}
-        </p>
+        />
+
+        <LogoOrEmoji path={path} />
+        <PageTitle path={path} title={title} />
+        <Name path={path} />
       </div>
     ),
     {
