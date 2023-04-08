97602631b4746cd65f9eea02a288b7db7a2af825
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2023-04-08 01:13:09 -0500

    feat: initial middleware implementation to capture country visits

---
+++ src/middleware.ts
@@ -0,0 +1,47 @@
+import { NextResponse } from 'next/server';
+import type { NextFetchEvent, NextRequest } from 'next/server';
+
+const stringToId = (text: string) => {
+  const noSpecialChars = text.toLowerCase().replace(/[^a-zA-Z ]/g, '--');
+  return noSpecialChars.split(' ').join('-');
+};
+
+export function middleware(request: NextRequest, event: NextFetchEvent) {
+  const { nextUrl: url, geo } = request;
+  const { pathname } = url;
+  const country = geo?.country || 'US';
+  const city = geo?.city || 'San Francisco';
+
+  // Ignore files and API calls
+  if (pathname.includes('.') || pathname.startsWith('/api')) {
+    return NextResponse.next();
+  }
+
+  event.waitUntil(
+    (async () => {
+      // Add view to your database
+      // ...
+      const cityAndCountry = `${city}, ${country}`;
+      console.error(`New visit from ${cityAndCountry}`);
+      console.error(`entry id: ${stringToId(cityAndCountry)}`);
+    })(),
+  );
+
+  // Ignore if not in the home page
+  if (pathname !== '/') return NextResponse.next();
+  url.searchParams.set('country', country);
+  return NextResponse.rewrite(url);
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+};
