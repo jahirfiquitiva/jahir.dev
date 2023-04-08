6a1bb894663782379f6d70467e8082242ae92789
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2023-04-07 20:13:19 -0500

    feat: apply zoom styles on tablets only

--- src/components/mdx/zoomable-img/zoomable-img.styles.ts
+++ src/components/mdx/zoomable-img/zoomable-img.styles.ts
@@ -3,12 +3,14 @@ import { styled } from '~/stitches';
 
 export const StyledImg = styled(Img, {
   '&[data-zoomable="true"]': {
-    cursor: 'zoom-in',
     canHover: {
-      '&.zoomed': {
-        cursor: 'zoom-out',
-        mx: '-$64',
-        minWidth: 'calc(100% + calc($space$64 * 2))',
+      '@tablet-lg': {
+        cursor: 'zoom-in',
+        '&.zoomed': {
+          cursor: 'zoom-out',
+          mx: '-$64',
+          minWidth: 'calc(100% + calc($space$64 * 2))',
+        },
       },
     },
   },
