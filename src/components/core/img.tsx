3204dc9c32c624aab8571ff0676fbc9e8da66783
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2023-04-09 17:31:51 -0500

    feat: create img component

---
+++ src/components/core/img.tsx
@@ -0,0 +1,37 @@
+import Image, { type ImageProps } from 'next/image';
+
+import tw from '@/tw';
+import type { FC } from '@/types';
+
+export const StyledImg = tw(Image)`
+  object-cover
+  object-center
+  text-transparent
+`;
+
+type BaseImageProps = Omit<ImageProps, 'width' | 'height'>;
+type SizeProps = BaseImageProps & { size?: number | string };
+type WidthHeightProps = BaseImageProps & {
+  width?: number | string;
+  height?: number | string;
+};
+
+export type ImgProps = SizeProps | WidthHeightProps;
+
+export const Img: FC<ImgProps> = (props) => {
+  const { size = 0, ...otherProps } = props as SizeProps;
+  const {
+    width = size,
+    height = size,
+    ...rest
+  } = otherProps as WidthHeightProps;
+  return (
+    <StyledImg
+      {...rest}
+      width={Number(width)}
+      height={Number(height)}
+      loading={props.priority ? undefined : props.loading || 'lazy'}
+      decoding={'async'}
+    />
+  );
+};
