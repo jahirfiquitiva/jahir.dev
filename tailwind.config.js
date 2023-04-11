54a5596523753ddf6be3825284e228e843b6982e
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2023-04-11 02:09:48 -0500

    chore: organize tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,79 @@
-import config from './tw-config/tw-config';
+import { fontFamily } from 'tailwindcss/defaultTheme';
 
-module.exports = config
+import { colors } from './tw-config/colors';
+import { fontSizes as fontSize } from './tw-config/font-sizes';
+import { spaces as spacing } from './tw-config/spacing';
+
+const sansFontFamily = ['var(--font-inter)', ...fontFamily.sans];
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/app/**/*.{js,ts,jsx,tsx}',
+    './content/**/*.mdx',
+  ],
+  theme: {
+    spacing,
+    borderRadius: { ...spacing, none: 0, half: '50%', full: '9999px' },
+    colors,
+    fontSize,
+    screens: {
+      default: '0px',
+      'mobile-sm': '320px',
+      'mobile-md': '375px',
+      'mobile-lg': '425px',
+      'tablet-sm': '596px',
+      'tablet-md': '768px',
+      'tablet-lg': '792px',
+      desktop: '960px',
+    },
+    extend: {
+      fontFamily: {
+        sans: sansFontFamily,
+        manrope: ['var(--font-manrope)', ...sansFontFamily],
+      },
+      fontWeight: {
+        normal: '450',
+      },
+      saturate: {
+        125: '1.25',
+      },
+      transitionTimingFunction: { eio: 'ease-in-out', DEFAULT: 'ease-in-out' },
+      keyframes: {
+        'page-transition': {
+          '0%': { transform: 'scale(0.975)', opacity: 0 },
+          '100%': { transform: 'scale(1)', opacity: 1 },
+        },
+        scroll: {
+          '0%': { transform: 'translateX(1.5rem)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
+      animation: {
+        'page-transition': 'page-transition 300ms ease-in-out backwards',
+        scroll: 'scroll 15s linear infinite',
+      },
+    },
+  },
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+  corePlugins: {
+    float: false,
+  },
+  plugins: [require('@tailwindcss/typography'), require('tailwindcss-hocus')],
+  safelist: [
+    {
+      pattern: /(from|to)-gradient-(brand|blue|green|yellow|orange|red|purple)/,
+      variants: ['dark', 'hocus', 'group-hocus/link'],
+    },
+    {
+      pattern:
+        /decoration-gradient-(brand|blue|green|yellow|orange|red|purple)/,
+      variants: ['hocus'],
+    },
+  ],
+};
