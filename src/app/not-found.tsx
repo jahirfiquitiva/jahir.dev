4b19b0e8b052da8843e64917a1e0c5bc610e09c7
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2023-04-13 17:52:29 -0500

    feat: improve not found page design

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,38 @@
+/* eslint-disable @next/next/no-img-element */
+import { cx } from 'classix';
+
+import { Heading } from '@/components/core/heading';
+import { ButtonLink } from '@/components/core/link';
+import { Section } from '@/components/core/section';
 import { getStaticMetadata } from '@/utils/metadata';
 import { buildOgImageUrl } from '@/utils/og';
 
 const NotFound = () => {
   return (
     <div className={'flex flex-col w-full flex-1 items-center justify-center'}>
-      <p>Not found</p>
+      <Section id={'error'} className={'w-full h-full'}>
+        <Heading shadow={'red'} from={'orange'} to={'red'} className={'mb-16'}>
+          Woops! ~ Page not found
+        </Heading>
+        <p>
+          Unfortunately, the page you&apos;re looking for doesn&apos;t exist or
+          has been moved.
+        </p>
+        <p>Please double check the URL for typos. Otherwise,</p>
+        <ButtonLink href={'/'} title={'Home page'}>
+          Go back home
+        </ButtonLink>
+        <img
+          src={'/static/images/site/404.gif'}
+          alt={'John Travolta gif'}
+          loading={'lazy'}
+          decoding={'async'}
+          className={cx(
+            'drop-shadow-[0_0_2px_#fff] mt-auto mx-auto w-full max-w-[425px]',
+            'mb-[calc(var(--verticalContentPadding)*-1)]',
+          )}
+        />
+      </Section>
     </div>
   );
 };
