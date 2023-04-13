c6a9577b854d303d4c57245e1bedda85eddcaa61
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2023-04-13 17:22:09 -0500

    feat: add metadata for 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,6 @@
+import { getStaticMetadata } from '@/utils/metadata';
+import { buildOgImageUrl } from '@/utils/og';
+
 const NotFound = () => {
   return (
     <div className={'flex flex-col w-full flex-1 items-center justify-center'}>
@@ -7,3 +10,10 @@ const NotFound = () => {
 };
 
 export default NotFound;
+
+export const metadata = getStaticMetadata({
+  title: 'Page not found',
+  description: "The page you're looking for doesn't exist or has been moved.",
+  keywords: ['404', 'not found', 'page not found'],
+  image: buildOgImageUrl('404'),
+});
