c15a32cf00c94b6ddb7e8e3d134b4deb11e50e24
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2023-04-19 15:29:47 -0500

    chore: add log for checking built page

--- src/utils/blogs.ts
+++ src/utils/blogs.ts
@@ -6,5 +6,9 @@ export const allReadableBlogs = generatedBlogs.filter(
   (it) => !hiddenBlogs.includes(it.slug),
 );
 
-export const getBlog = (slug?: string | null): Blog | undefined =>
-  slug ? generatedBlogs.find((it) => it.slug === slug) : undefined;
+export const getBlog = (slug?: string | null): Blog | undefined => {
+  const blog = slug ? generatedBlogs.find((it) => it.slug === slug) : undefined;
+  console.error(`getting blog with slug: ${slug}`);
+  console.error(`blog found ? [${blog?.title}]`);
+  return blog;
+};
