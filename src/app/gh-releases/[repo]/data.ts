e746492e00aede2838f019b420074334dc932aed
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2023-04-19 23:19:59 -0500

    feat: create github releases page

---
+++ src/app/gh-releases/[repo]/data.ts
@@ -0,0 +1,29 @@
+const { GITHUB_API_TOKEN: githubApiToken = '' } = process.env;
+const authHeaders =
+  githubApiToken && githubApiToken.length > 0
+    ? { headers: { Authorization: `token ${githubApiToken}` } }
+    : {};
+
+export const getRepoReleaseData = async (name?: string) => {
+  if (!name || !name.length) return null;
+  const dataRequest = await fetch(
+    `https://api.github.com/repos/jahirfiquitiva/${name}/releases/latest`,
+    authHeaders,
+  );
+  const data = await dataRequest.json();
+  const { assets = [] } = data;
+  const extraRepoData = {
+    success: false,
+    url: `https://github.com/jahirfiquitiva/${name}`,
+  };
+  const defaultDownloadLink = `https://github.com/jahirfiquitiva/${name}/releases/latest/`;
+  let downloadLink = defaultDownloadLink;
+  if (assets) {
+    const [apk] = assets;
+    if (apk) {
+      extraRepoData.success = true;
+      downloadLink = apk.browser_download_url || defaultDownloadLink;
+    }
+  }
+  return { ...extraRepoData, download: downloadLink };
+};
