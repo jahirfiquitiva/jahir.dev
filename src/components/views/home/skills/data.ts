38e361ca72bd15a339934038cdb54e2031293224
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2023-04-29 20:02:18 -0500

    chore: update skills order

--- src/components/views/home/skills/data.ts
+++ src/components/views/home/skills/data.ts
@@ -25,12 +25,8 @@ export interface SkillProps {
 }
 
 export const skills: SkillProps[] = [
-  { name: 'Android', iconPath: mdiAndroid, color: '#3ddc84' },
-  { name: 'Kotlin', iconPath: mdiLanguageKotlin, color: '#6677e0' },
-  { name: 'JavaScript', iconPath: mdiLanguageJavascript, color: '#f7df1e' },
   { name: 'TypeScript', iconPath: mdiLanguageTypescript, color: '#3178c6' },
-  { name: 'HTML5', iconPath: mdiLanguageHtml5, color: '#e34f26' },
-  { name: 'CSS3', iconPath: mdiLanguageCss3, color: '#3572b5' },
+  { name: 'JavaScript', iconPath: mdiLanguageJavascript, color: '#f7df1e' },
   { name: 'React', iconPath: mdiReact, color: '#00c2e6' },
   { name: 'Next.js', iconPath: nextJs, color: '#0070f3' },
   {
@@ -39,6 +35,10 @@ export const skills: SkillProps[] = [
     color: '#c43bad',
   },
   { name: 'Tailwind CSS', iconPath: mdiTailwind, color: '#38bdf8' },
+  { name: 'HTML5', iconPath: mdiLanguageHtml5, color: '#e34f26' },
+  { name: 'CSS3', iconPath: mdiLanguageCss3, color: '#3572b5' },
+  { name: 'Android', iconPath: mdiAndroid, color: '#3ddc84' },
+  { name: 'Kotlin', iconPath: mdiLanguageKotlin, color: '#6677e0' },
   { name: 'Node.js', iconPath: mdiNodejs, color: '#61af43' },
   { name: 'Mongo DB', iconPath: mdiLeaf, color: '#69a14a' },
   { name: 'Express', iconPath: mdiPuzzleOutline, color: '#888888' },
