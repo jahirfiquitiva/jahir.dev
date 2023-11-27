6a4adf982f84aee234504b8580ad09872077fedf
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2023-11-26 22:43:20 -0500

    feat: update instagram api and types to support carousel and video

--- src/lib/instagram/instagram.ts
+++ src/lib/instagram/instagram.ts
@@ -1,46 +1,32 @@
 import { getPublicFeed } from './public';
-import type { RemoteInstagramPost, BeholdPost, InstagramPost } from './types.d';
+import type { RemoteInstagramPost, InstagramPost } from './types.d';
 
 const instagramApi = process.env.INSTA_API || '';
 const instagramFeedKey = process.env.INSTA_FEED_KEY || '';
 const instagramFeedBackupKey = process.env.INSTA_FEED_KEY_BKP || '';
 
 const fetchSelfFeed = async (): Promise<Array<InstagramPost>> => {
-  const response = await fetch(`${instagramApi}/posts`);
-  const data: { posts?: Array<RemoteInstagramPost> } = await response.json();
-  return (data.posts || []).map(
-    (post) =>
-      ({
-        id: post.id,
-        caption: post.caption,
-        postUrl: post.permalink,
-        photoUrl: `https://sroc.jahir.dev/${post.photo || post.mediaUrl}`,
-      }) as InstagramPost,
-  );
+  try {
+    const response = await fetch(`${instagramApi}/posts`);
+    const data: { posts?: Array<RemoteInstagramPost> } = await response.json();
+    return data.posts || [];
+  } catch (e) {
+    return [];
+  }
 };
 
 const fetchBeholdFeed = async (key?: string): Promise<Array<InstagramPost>> => {
   if (!key) return [];
   try {
     const response = await fetch(`https://feeds.behold.so/${key}`);
-    const data: Array<BeholdPost> = await response.json();
-    return (data || []).map(
-      (post) =>
-        ({
-          id: post.id,
-          caption: post.prunedCaption || post.caption,
-          postUrl: post.permalink,
-          photoUrl: post.thumbnailUrl || post.mediaUrl,
-          colorPalette: post.colorPalette,
-          dimensions: post.dimensions,
-        }) as InstagramPost,
-    );
+    const data: Array<RemoteInstagramPost> = await response.json();
+    return data;
   } catch (e) {
     return [];
   }
 };
 
-export const fetchInstaFeed = async (): Promise<Array<BeholdPost>> => {
+export const fetchInstaFeed = async (): Promise<Array<InstagramPost>> => {
   try {
     const mainFeed = await fetchBeholdFeed(instagramFeedKey);
     if (mainFeed && mainFeed.length) return mainFeed;
