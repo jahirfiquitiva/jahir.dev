d5a6992e3ebac70180961e3a6794e43c5d8166a4
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2023-12-05 23:01:55 -0500

    chore: remove blog post section from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,6 @@
-import { BlogPostCard } from '@/components/views/blog/card/card';
 import { Intro } from '@/components/views/home/intro/intro';
 import { Skills } from '@/components/views/home/skills/skills';
-import { ViewsCounter } from '@/components/views/mdx/ui/views/counter';
-import { LatestBlogPost } from '@/components/views/projects/latest-blog';
 import { Projects } from '@/components/views/projects/projects';
-import { allReadableBlogs } from '@/utils/blog';
-
-const allowInProgress = process.env.NODE_ENV === 'development';
-const [latestBlogPost] = allReadableBlogs
-  .filter((it) => allowInProgress || !it.inProgress)
-  .sort((a, b) => Number(new Date(b.date)) - Number(new Date(a.date)));
 
 export default function Home() {
   return (
@@ -17,19 +8,6 @@ export default function Home() {
       <Intro />
       <Projects />
       <Skills />
-      <LatestBlogPost>
-        <BlogPostCard
-          post={latestBlogPost}
-          viewsCounter={
-            <ViewsCounter
-              slug={`blog--${latestBlogPost.slug}`}
-              inProgress={latestBlogPost.inProgress}
-              $sm
-            />
-          }
-          showYear
-        />
-      </LatestBlogPost>
     </>
   );
 }
