d55a6edb82ff9b37fe8c8a736251f85932c67de7
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2023-12-11 17:36:19 -0500

    chore: implement new vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import '@/styles/globals.scss';
 
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Inter, Manrope } from 'next/font/google';
 import Script from 'next/script';
 import { type PropsWithChildren } from 'react';
@@ -76,6 +77,7 @@ export default function RootLayout(props: PropsWithChildren) {
           <Footer />
           <BackToTop />
         </Providers>
+        <SpeedInsights />
       </body>
     </html>
   );
