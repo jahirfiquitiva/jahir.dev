b447d8cd73707fb1f430cca8487c4ec9bffec4b8
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2023-12-13 00:36:18 -0500

    feat: create latest blog posts section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,45 @@
+import { BlogPostCard } from '@/components/views/blog/card/card';
+import { LatestBlogPost } from '@/components/views/blog/latest-blog';
 import { Intro } from '@/components/views/home/intro/intro';
 import { LetsConnect } from '@/components/views/home/lets-connect';
+import { ViewsCounter } from '@/components/views/mdx/ui/views/counter';
 import { Projects } from '@/components/views/projects/projects';
+import { allReadableBlogs } from '@/utils/blog';
+
+const allowInProgress = process.env.NODE_ENV === 'development';
+const [latestBlogPost, latestBlogPostB] = allReadableBlogs
+  .filter((it) => allowInProgress || !it.inProgress)
+  .sort((a, b) => Number(new Date(b.date)) - Number(new Date(a.date)));
 
 export default function Home() {
   return (
     <>
       <Intro />
       <LetsConnect />
+      <LatestBlogPost>
+        <BlogPostCard
+          post={latestBlogPost}
+          viewsCounter={
+            <ViewsCounter
+              slug={`blog--${latestBlogPost.slug}`}
+              inProgress={latestBlogPost.inProgress}
+              $sm
+            />
+          }
+          showYear
+        />
+        <BlogPostCard
+          post={latestBlogPostB}
+          viewsCounter={
+            <ViewsCounter
+              slug={`blog--${latestBlogPostB.slug}`}
+              inProgress={latestBlogPostB.inProgress}
+              $sm
+            />
+          }
+          showYear
+        />
+      </LatestBlogPost>
       <Projects />
     </>
   );
--- src/components/views/home/lets-connect.tsx
+++ src/components/views/home/lets-connect.tsx
@@ -1,11 +1,12 @@
+import { Section } from '@/components/core/section';
 import { SocialLinks } from '@/components/molecules/social-links/social-links';
 import cx from '@/utils/cx';
 
 export const LetsConnect = () => {
   return (
-    <section id={'contact'} className={cx('flex flex-col gap-6 -mt-20')}>
+    <Section id={'contact'} className={cx('gap-6 -mt-20')}>
       <p className={'text-2xs font-manrope font-bold'}>Let&apos;s connect!</p>
       <SocialLinks className={'justify-start'} />
-    </section>
+    </Section>
   );
 };
