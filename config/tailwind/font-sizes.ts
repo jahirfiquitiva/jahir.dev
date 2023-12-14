9e392c91bb69a139259e030ae557e9410dace53a
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2023-12-14 13:24:20 -0500

    feat: fix issue with site font-size being overriden

--- config/tailwind/font-sizes.ts
+++ config/tailwind/font-sizes.ts
@@ -1,12 +1,12 @@
 export const fontSizes = {
-  '3xl': '26px', // h1
-  '2xl': '24px', // h2
-  xl: '22px', // h3
-  lg: '20px', // h4
-  md: '18px', // h5
-  sm: '16px', // h6
-  xs: '15px', // body, p
-  '2xs': '14px', // small
-  '3xs': '13px', // (?)
-  '4xs': '12px', // (?)
+  '3xl': '1.625rem', // h1
+  '2xl': '1.5rem', // h2
+  xl: '1.375rem', // h3
+  lg: '1.25rem', // h4
+  md: '1.125rem', // h5
+  sm: '1rem', // h6
+  xs: '0.9375rem', // body, p
+  '2xs': '0.875rem', // small
+  '3xs': '0.8125rem', // (?)
+  '4xs': '0.75rem', // (?)
 };
