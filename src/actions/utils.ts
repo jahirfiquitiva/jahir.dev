f194a4b2a3a60a165d0eaabacaab378d0ba7979a
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2023-12-27 23:08:48 -0500

    feat: be more restrictive for when to run actions

--- src/actions/utils.ts
+++ src/actions/utils.ts
@@ -1,3 +1,13 @@
 const PROD_ENV = 'production';
+
+const {
+  NODE_ENV = 'development',
+  VERCEL_ENV = 'development',
+  VERCEL_GIT_REPO_SLUG = '',
+  VERCEL_GIT_REPO_OWNER = '',
+} = process.env;
+
 export const canRunAction =
-  process.env.NODE_ENV === PROD_ENV || process.env.VERCEL_ENV === PROD_ENV;
+  (NODE_ENV === PROD_ENV || VERCEL_ENV === PROD_ENV) &&
+  VERCEL_GIT_REPO_OWNER === 'jahirfiquitiva' &&
+  VERCEL_GIT_REPO_SLUG === 'jahir.dev';
