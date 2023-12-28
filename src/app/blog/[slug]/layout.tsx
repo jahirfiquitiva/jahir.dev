ee0c4a81e5af28a2ca2a13cc39f391ad3dd28126
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2023-12-27 23:05:38 -0500

    feat: update blog post buttons icons and url

--- src/app/blog/[slug]/layout.tsx
+++ src/app/blog/[slug]/layout.tsx
@@ -70,14 +70,14 @@ export default async function BlogPostLayout(
           <ShareButton title={'Share blog post'} slug={slug || ''} />
           <OutlinedLinkButton
             title={'Edit blog post'}
-            href={`https://github.com/jahirfiquitiva/jahir.dev/edit/main/src/content/blog/${slug}/index.mdx`}
+            href={`https://github.com/jahirfiquitiva/jahir.dev/edit/main/content/${slug}/index.mdx`}
             className={'pr-4'}
           >
             <Icon
               className={'size-5'}
               path={
                 // eslint-disable-next-line max-len
-                'M14.06,9L15,9.94L5.92,19H5V18.08L14.06,9M17.66,3C17.41,3 17.15,3.1 16.96,3.29L15.13,5.12L18.88,8.87L20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18.17,3.09 17.92,3 17.66,3M14.06,6.19L3,17.25V21H6.75L17.81,9.94L14.06,6.19Z'
+                'M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z'
               }
             />
             <span>Edit on GitHub</span>
