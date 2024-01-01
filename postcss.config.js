d2d9ac3b6892a9e249b36c2ae453293c99723598
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2024-01-01 18:06:24 -0500

    chore: update dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
+    cssnano: {},
+    // ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
   },
 };
