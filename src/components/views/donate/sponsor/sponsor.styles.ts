246636d831a88307b95a4d745c0c95ac352f2d94
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2024-01-02 04:05:05 -0500

    feat: minor design tweak for sponsors tier badge

--- src/components/views/donate/sponsor/sponsor.styles.ts
+++ src/components/views/donate/sponsor/sponsor.styles.ts
@@ -26,7 +26,7 @@ export const NameAndPhotoContainer = tw.div`
 
 export const Tier = tw.span`
   flex items-center gap-1
-  rounded-2 pl-2 pr-2.5 py-0.5 min-h-9
+  rounded-2 pl-2 pr-2.5 min-h-9
   border border-divider
   text-3xs font-medium cursor-default
   transition-colors
