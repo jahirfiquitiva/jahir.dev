3cc9589f5e6f03c1aec72b477d8df44a8bfe8275
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2024-01-05 09:12:03 -0500

    feat: make image component a client component with loading transition

--- src/components/atoms/img.tsx
+++ src/components/atoms/img.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import Image, { type ImageProps } from 'next/image';
+import { useState } from 'react';
 
 import cx, { tw } from '@/utils/cx';
 
@@ -12,6 +15,7 @@ type WidthHeightProps = BaseImageProps & {
 export type ImgProps = SizeProps | WidthHeightProps;
 
 const BaseImg = (props: ImgProps) => {
+  const [isLoading, setLoading] = useState(true);
   const { size = 0, ...otherProps } = props as SizeProps;
   const {
     width = size,
@@ -24,9 +28,16 @@ const BaseImg = (props: ImgProps) => {
       alt={rest.alt}
       width={Number(width)}
       height={Number(height)}
-      loading={rest.priority ? undefined : rest.loading || 'lazy'}
-      decoding={'async'}
-      className={cx('object-cover object-center', rest.className)}
+      placeholder={typeof rest.src !== 'string' ? 'blur' : undefined}
+      className={cx(
+        'object-cover object-center',
+        'transition-[scale,filter] duration-500',
+        isLoading ? 'grayscale blur-md scale-105' : '',
+        rest.className,
+      )}
+      onLoad={() => {
+        setLoading(false);
+      }}
     />
   );
 };
