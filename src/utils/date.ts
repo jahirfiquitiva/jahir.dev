1a46621cf157753a53e380e2babf79fe492af9cf
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2024-01-04 20:54:52 -0500

    feat: improve and simplify date formatting

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -10,14 +10,24 @@ const dateFormattingOptions: Intl.DateTimeFormatOptions = {
   year: 'numeric',
 };
 
+const getDate = (baseDate?: string | Date): Date | null => {
+  if (!baseDate) return null;
+  if (typeof baseDate !== 'string') return baseDate;
+  if (!baseDate.includes('T')) {
+    // Use Colombia Timezone (UTC-5)
+    baseDate = `${baseDate}T00:00:00-05:00`;
+  }
+  return new Date(baseDate);
+};
+
 export const formatDate = (
-  preDate?: string | Date,
+  baseDate?: string | Date,
   asTime?: boolean,
   options: Intl.DateTimeFormatOptions | undefined | null = {},
 ): string => {
-  if (!preDate) return '';
   try {
-    const date = typeof preDate === 'string' ? new Date(preDate) : preDate;
+    const date = getDate(baseDate);
+    if (!date) return '';
     return new Intl.DateTimeFormat('en-US', {
       timeZone: 'America/Bogota',
       ...(asTime ? timeFormattingOptions : dateFormattingOptions),
