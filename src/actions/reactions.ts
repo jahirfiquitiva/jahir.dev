291846c60082d6d91dab172dbe0d6ffedbf2e30d
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2024-01-10 17:08:19 -0500

    feat: add cache back to server actions

--- src/actions/reactions.ts
+++ src/actions/reactions.ts
@@ -32,25 +32,25 @@ export const incrementReaction = cache(
   },
 );
 
-export const getReactions = async (
-  slug: string,
-): Promise<ReactionsCounters> => {
-  noStore();
-  try {
-    const data = await db
-      .selectFrom('counters')
-      .where('slug', '=', slug)
-      .select(['likes', 'loves', 'awards', 'bookmarks'])
-      .execute();
-    const [counters] = data;
-    if (!counters) return {};
-    return {
-      likes: counters.likes || 0,
-      loves: counters.loves || 0,
-      awards: counters.awards || 0,
-      bookmarks: counters.bookmarks || 0,
-    };
-  } catch (e) {
-    return {};
-  }
-};
+export const getReactions = cache(
+  async (slug: string): Promise<ReactionsCounters> => {
+    noStore();
+    try {
+      const data = await db
+        .selectFrom('counters')
+        .where('slug', '=', slug)
+        .select(['likes', 'loves', 'awards', 'bookmarks'])
+        .execute();
+      const [counters] = data;
+      if (!counters) return {};
+      return {
+        likes: counters.likes || 0,
+        loves: counters.loves || 0,
+        awards: counters.awards || 0,
+        bookmarks: counters.bookmarks || 0,
+      };
+    } catch (e) {
+      return {};
+    }
+  },
+);
--- src/actions/stars.tsx
+++ src/actions/stars.tsx
@@ -1,4 +1,5 @@
 import { unstable_noStore as noStore } from 'next/cache';
+import { cache } from 'react';
 
 const gitHubApiUrl = 'https://api.github.com';
 const { GITHUB_API_TOKEN: githubApiToken = '' } = process.env;
@@ -10,31 +11,30 @@ const authHeaders =
 const oneMillion = 1000000;
 const oneThousand = 1000;
 
-export const getRepoStars = async (
-  repo: string,
-  owner?: string,
-): Promise<string | undefined> => {
-  if (!repo || !repo.length) return undefined;
-  noStore();
-  try {
-    const repoRequest = await fetch(
-      `${gitHubApiUrl}/repos/${owner || 'jahirfiquitiva'}/${repo}`,
-      authHeaders,
-    );
+export const getRepoStars = cache(
+  async (repo: string, owner?: string): Promise<string | undefined> => {
+    if (!repo || !repo.length) return undefined;
+    noStore();
+    try {
+      const repoRequest = await fetch(
+        `${gitHubApiUrl}/repos/${owner || 'jahirfiquitiva'}/${repo}`,
+        authHeaders,
+      );
 
-    if (!repoRequest.ok) return undefined;
-    const repository = await repoRequest.json();
-    const { stargazers_count: stargazers = 0 } = repository;
-    if (stargazers <= 0) return undefined;
+      if (!repoRequest.ok) return undefined;
+      const repository = await repoRequest.json();
+      const { stargazers_count: stargazers = 0 } = repository;
+      if (stargazers <= 0) return undefined;
 
-    let starsAsText = '';
-    if (stargazers >= oneMillion) {
-      starsAsText = `${Math.floor(stargazers / oneMillion)}M+`;
-    } else if (stargazers >= oneThousand) {
-      starsAsText = `${Math.floor(stargazers / oneThousand)}K+`;
-    } else starsAsText = `${stargazers}`;
-    return starsAsText;
-  } catch (e) {
-    return undefined;
-  }
-};
+      let starsAsText = '';
+      if (stargazers >= oneMillion) {
+        starsAsText = `${Math.floor(stargazers / oneMillion)}M+`;
+      } else if (stargazers >= oneThousand) {
+        starsAsText = `${Math.floor(stargazers / oneThousand)}K+`;
+      } else starsAsText = `${stargazers}`;
+      return starsAsText;
+    } catch (e) {
+      return undefined;
+    }
+  },
+);
