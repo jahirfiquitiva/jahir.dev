a587ba5e2c9a92bf8cdafc6ac1049e36070ee815
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2024-01-14 01:10:09 -0500

    fix: incorrect type

--- config/mdx/index.ts
+++ config/mdx/index.ts
@@ -1,4 +1,4 @@
-import type { MDXOptions } from 'contentlayer/core';
+import type { SerializeOptions } from 'next-mdx-remote/dist/types';
 import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import rehypeSlug from 'rehype-slug';
@@ -9,7 +9,7 @@ import remarkUnwrapImages from 'remark-unwrap-images';
 import { prettyCode } from './rehype/code';
 import { toc } from './rehype/toc';
 
-const mdx: MDXOptions = {
+const mdx: SerializeOptions['mdxOptions'] = {
   remarkPlugins: [remarkGfm, remarkSqueezeParagraphs, remarkUnwrapImages],
   rehypePlugins: [
     rehypeSlug,
