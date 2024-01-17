7b4f50e029d03bbc4c218370d3b1ffd3b852230b
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2024-01-17 10:24:15 -0500

    feat: make stars count a client component

--- src/components/ui/projects/item/index.tsx
+++ src/components/ui/projects/item/index.tsx
@@ -5,9 +5,8 @@ import type { Project } from '@/types/project';
 import { hexToRgb } from '@/utils/color';
 import cx from '@/utils/cx';
 
-// import { StarsCount } from '../stars-count';
-
 import { ProjectIcon, ProjectLink } from './item.styles';
+import { StarsCount } from './stars-count';
 
 interface ProjectItemProps {
   project: Project;
@@ -46,7 +45,7 @@ export const ProjectItem = ({ project }: ProjectItemProps) => {
           >
             {project.name}
           </p>
-          {/* <StarsCount repo={project.repo || ''} owner={project.owner} /> */}
+          <StarsCount repo={project.repo || ''} owner={project.owner} />
         </div>
         <p className={'text-2xs text-secondary-txt line-clamp-2 text-pretty'}>
           {project.description}
