0e8a7beb4f5b7ed7e41766f6ff3b6d9352546e19
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2024-01-18 12:33:31 -0500

    feat: add main component to error and not-found pages to fix layout issues

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import { LinkButton } from '@/components/atoms/link-button';
+import { Main } from '@/components/atoms/main';
 import { Section } from '@/components/atoms/section';
 import { getColoredTextClasses } from '@/utils/colored-text';
 import cx from '@/utils/cx';
@@ -7,40 +8,42 @@ import { createMetadata } from '@/utils/metadata';
 
 export default function NotFound() {
   return (
-    <Section
-      id={'error'}
-      className={'flex-1 w-full h-full -mb-8 tablet-sm:-mb-9'}
-    >
-      <h1
-        className={getColoredTextClasses(
-          'red',
-          'orange',
-          'red',
-          'mb-4 self-start',
-        )}
+    <Main>
+      <Section
+        id={'error'}
+        className={'flex-1 w-full h-full -mb-7 tablet-sm:-mb-8'}
       >
-        Woops! ~ Page not found
-      </h1>
-      <p>
-        Unfortunately, the page you&apos;re looking for doesn&apos;t exist or
-        has been moved.
-      </p>
-      <p className={'-mt-3'}>
-        Please double check the URL for typos. Otherwise,
-      </p>
-      <LinkButton href={'/'} title={'Home page'} className={'mt-1.5 mb-5'}>
-        Go back home
-      </LinkButton>
-      <img
-        src={'/media/site/404.gif'}
-        alt={'John Travolta gif'}
-        loading={'lazy'}
-        decoding={'async'}
-        className={cx(
-          'drop-shadow-[0_0_2px_#fff] mt-auto mx-auto w-full max-w-[425px]',
-        )}
-      />
-    </Section>
+        <h1
+          className={getColoredTextClasses(
+            'red',
+            'orange',
+            'red',
+            'mb-4 self-start',
+          )}
+        >
+          Woops! ~ Page not found
+        </h1>
+        <p>
+          Unfortunately, the page you&apos;re looking for doesn&apos;t exist or
+          has been moved.
+        </p>
+        <p className={'-mt-3'}>
+          Please double check the URL for typos. Otherwise,
+        </p>
+        <LinkButton href={'/'} title={'Home page'} className={'mt-1.5 mb-5'}>
+          Go back home
+        </LinkButton>
+        <img
+          src={'/media/site/404.gif'}
+          alt={'John Travolta gif'}
+          loading={'lazy'}
+          decoding={'async'}
+          className={cx(
+            'drop-shadow-[0_0_2px_#fff] mt-auto mx-auto w-full max-w-[425px]',
+          )}
+        />
+      </Section>
+    </Main>
   );
 }
 
