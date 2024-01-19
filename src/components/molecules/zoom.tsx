11f4ff2a13f0f66e47ca74e416637c2859cb1ca2
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2024-01-19 17:18:03 -0500

    feat: load zoom only after component is mounted

--- src/components/molecules/zoom.tsx
+++ src/components/molecules/zoom.tsx
@@ -3,8 +3,12 @@
 import mediumZoom from 'medium-zoom';
 import { useEffect } from 'react';
 
+import { useHasMounted } from '@/hooks/use-has-mounted';
+
 export const Zoom = () => {
+  const hasMounted = useHasMounted();
   useEffect(() => {
+    if (!hasMounted) return;
     const zoom = mediumZoom(
       'img:not([data-nozoom]):not(a img):not(.image-comparison img)',
     );
