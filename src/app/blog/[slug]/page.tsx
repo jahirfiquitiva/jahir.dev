b709b1e7cedf33fa6b8aa47fcd35dec61b5473d3
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2024-01-20 17:08:51 -0500

    chore: remove unnecessary elements from blog post page

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,26 +1,18 @@
 import type { Metadata } from 'next';
-import { redirect } from 'next/navigation';
-import { Suspense } from 'react';
+import { notFound, redirect } from 'next/navigation';
 
 import { Mdx } from '@/components/ui/blog/mdx';
 import { allReadableBlogs } from '@/utils/blog';
 import { createMetadata } from '@/utils/metadata';
 
-import Loading from './../../loading';
-import NotFound from './../../not-found';
 import type { BlogPostPageContext } from './types';
 
 export default function BlogPostPage(context: BlogPostPageContext) {
   const { slug } = context.params;
   const post = allReadableBlogs.find((b) => b.slug === slug);
-
-  if (!slug || !post) return <NotFound />;
+  if (!slug || !post) return notFound();
   if (post.link) return redirect(post.link);
-  return (
-    <Suspense fallback={<Loading />}>
-      <Mdx code={post.body.code} />
-    </Suspense>
-  );
+  return <Mdx code={post.body.code} />;
 }
 
 export const generateStaticParams = () =>
