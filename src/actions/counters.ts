b3a53ea1703c50f8a8b066eea3c1027bb0277e06
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2024-02-14 12:50:58 -0500

    feat: do not use dynamic functions in counters

--- src/actions/counters.ts
+++ src/actions/counters.ts
@@ -1,8 +1,5 @@
 'use server';
 
-import { unstable_noStore as noStore } from 'next/cache';
-import { cache } from 'react';
-
 import {
   db,
   type CounterName,
@@ -12,31 +9,28 @@ import {
 
 import { canRunAction } from './utils';
 
-export const incrementCounter = cache(
-  async (
-    slug: string,
-    counter: CounterName,
-  ): Promise<{ [counter in CounterName]?: number }> => {
-    if (!canRunAction) return {};
-    noStore();
-    try {
-      const data = await db
-        .selectFrom('counters')
-        .where('slug', '=', slug)
-        .select([counter])
-        .execute();
+export const incrementCounter = async (
+  slug: string,
+  counter: CounterName,
+): Promise<{ [counter in CounterName]?: number }> => {
+  if (!canRunAction) return {};
+  try {
+    const data = await db
+      .selectFrom('counters')
+      .where('slug', '=', slug)
+      .select([counter])
+      .execute();
 
-      const counterCount = !data.length ? 0 : Number(data[0][counter]);
-      db.insertInto('counters')
-        .values({ slug, [counter]: 1 })
-        .onDuplicateKeyUpdate({ [counter]: counterCount + 1 })
-        .execute();
-      return { [counter]: counterCount + 1 };
-    } catch (e) {
-      return {};
-    }
-  },
-);
+    const counterCount = !data.length ? 0 : Number(data[0][counter]);
+    db.insertInto('counters')
+      .values({ slug, [counter]: 1 })
+      .onDuplicateKeyUpdate({ [counter]: counterCount + 1 })
+      .execute();
+    return { [counter]: counterCount + 1 };
+  } catch (e) {
+    return {};
+  }
+};
 
 export type IncrementCounterFnType = typeof incrementCounter;
 
@@ -44,7 +38,6 @@ export const getCounters = async (
   slug: string,
   counter?: CounterName,
 ): Promise<Counters> => {
-  noStore();
   try {
     const [counters] = await db
       .selectFrom('counters')
