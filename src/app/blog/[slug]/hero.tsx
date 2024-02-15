eef34479902cfa901339bffb78ba0e62351ee1b5
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2024-02-15 10:44:17 -0500

    style: slightly improve legibility of texts in blog posts header

--- src/app/blog/[slug]/hero.tsx
+++ src/app/blog/[slug]/hero.tsx
@@ -20,7 +20,7 @@ export const Hero = (props: HeroProps) => {
         'max-w-[calc(100vw_+_calc(100vw_-_100%))]',
         'absolute top-0 -left-[calc(100vw_-_100%)] -right-[calc(100vw_-_100%)]',
         'pointer-events-none blur transition',
-        'saturate-125 opacity-50 dark:opacity-65',
+        'saturate-125 opacity-40 dark:opacity-65',
         // eslint-disable-next-line max-len
         '[mask-image:linear-gradient(to_bottom,rgba(0,0,0,0.9)_0%,rgba(0,0,0,0)_100%)]',
       )}
