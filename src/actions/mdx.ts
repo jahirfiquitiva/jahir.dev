dfe7d5f818b237c0b19cd43d0d15da14c4680f29
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2024-02-17 16:43:32 -0500

    feat: add actions to safely get tweet and bookmark data for prerendering

---
+++ src/actions/mdx.ts
@@ -0,0 +1,46 @@
+import { unstable_noStore as noStore } from 'next/cache';
+import type { TweetProps } from 'react-tweet';
+import { getTweet as getTweetFromApi } from 'react-tweet/api';
+
+export const getTweet = async (
+  id?: TweetProps['id'],
+  onError?: TweetProps['onError'],
+) => {
+  if (!id) return { error: 'Missing tweet id' };
+  noStore();
+  try {
+    let error;
+    const tweet = id
+      ? await getTweetFromApi(id).catch((err: unknown) => {
+          if (onError) {
+            error = onError(err);
+          } else {
+            error = err;
+          }
+        })
+      : undefined;
+    return { tweet, error };
+  } catch (e) {
+    return { error: (e as Error).message };
+  }
+};
+
+interface SiteMetadata {
+  title: string;
+  description?: string;
+  image?: string;
+}
+
+export const getMetadata = async (url: string) => {
+  if (!url) return null;
+  try {
+    const req = await fetch(`https://api.dub.co/metatags?url=${url}`).catch(
+      null,
+    );
+    if (!req.ok) return null;
+    const data = await req.json();
+    return data as SiteMetadata;
+  } catch (e) {
+    return null;
+  }
+};
--- src/components/ui/blog/mdx/tweet/index.tsx
+++ src/components/ui/blog/mdx/tweet/index.tsx
@@ -1,22 +1,14 @@
+import { Suspense } from 'react';
 import { TweetNotFound, type TweetProps } from 'react-tweet';
-import { getTweet } from 'react-tweet/api';
+
+import { getTweet } from '@/actions/mdx';
 
 import { CustomTweet } from './custom-tweet';
+
 import './tweet.scss';
 
 const TweetContent = async ({ id, components, onError }: TweetProps) => {
-  let error;
-  const tweet = id
-    ? await getTweet(id).catch((err) => {
-        if (onError) {
-          error = onError(err);
-        } else {
-          console.error(err);
-          error = err;
-        }
-      })
-    : undefined;
-
+  const { tweet, error } = await getTweet(id, onError);
   if (!tweet) {
     const NotFound = components?.TweetNotFound || TweetNotFound;
     return <NotFound error={error} />;
@@ -24,4 +16,8 @@ const TweetContent = async ({ id, components, onError }: TweetProps) => {
   return <CustomTweet tweet={tweet} components={components} />;
 };
 
-export const ReactTweet = (props: TweetProps) => <TweetContent {...props} />;
+export const ReactTweet = (props: TweetProps) => (
+  <Suspense fallback={<div className={'min-w-11 min-h-11'} />}>
+    <TweetContent {...props} />
+  </Suspense>
+);
