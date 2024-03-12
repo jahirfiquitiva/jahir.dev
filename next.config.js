4bfa52a805af7ba5b47be6235f68c95613f5523d
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2024-03-12 15:06:48 -0500

    chore: update velite plugin config

--- next.config.js
+++ next.config.js
@@ -11,20 +11,15 @@ const redirects = require('./config/next/redirects');
 
 class VeliteWebpackPlugin {
   static started = false;
-  constructor(/** @type {import('velite').Options} */ options = {}) {
-    this.options = options;
-  }
   apply(/** @type {import('webpack').Compiler} */ compiler) {
-    // executed three times in nextjs !!!
+    // executed three times in nextjs
     // twice for the server (nodejs / edge runtime) and once for the client
     compiler.hooks.beforeCompile.tapPromise('VeliteWebpackPlugin', async () => {
       if (VeliteWebpackPlugin.started) return;
       VeliteWebpackPlugin.started = true;
       const dev = compiler.options.mode === 'development';
-      this.options.watch = this.options.watch ?? dev;
-      this.options.clean = this.options.clean ?? !dev;
       const { build } = await import('velite');
-      await build(this.options); // start velite
+      await build({ watch: dev, clean: !dev });
     });
   }
 }
