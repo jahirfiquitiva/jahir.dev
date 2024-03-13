e848e643f76ec15d8c6dedf73545346456bb2e82
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2024-03-13 09:09:49 -0500

    feat: prevent Flash Of Unstyled Text

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -3,14 +3,14 @@ import localFont from 'next/font/local';
 export const Inter = localFont({
   src: './../assets/fonts/InterVariable.woff2',
   variable: '--font-inter',
-  display: 'fallback',
+  display: 'swap',
   weight: '1 999',
 });
 
 export const Manrope = localFont({
   src: './../assets/fonts/ManropeVariable.woff2',
   variable: '--font-manrope',
-  display: 'fallback',
+  display: 'swap',
   weight: '1 999',
   preload: false,
 });
