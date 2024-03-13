5abae6d9b4e1c1ad7103329b3a8d8efab2554b72
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2024-03-12 20:18:43 -0500

    chore: revert last change

--- src/components/ui/projects/item/index.tsx
+++ src/components/ui/projects/item/index.tsx
@@ -27,7 +27,8 @@ export const ProjectItem = ({ project }: ProjectItemProps) => {
         src={`/media/projects/${project.icon || ''}`}
         alt={`Icon for project "${project.name}"`}
         size={56}
-        {...project.iconMeta}
+        blurDataURL={project.iconMeta?.blurDataURL}
+        placeholder={project.iconMeta?.placeholder}
       />
       <div className={'flex flex-col gap-0.5'}>
         <div className={'flex flex-row gap-3 items-center'}>
