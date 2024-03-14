f574b905b28384a876b98cb6c6a044b72b33bfc9
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2024-03-13 23:27:54 -0500

    feat: make img a client component to handle errors

--- src/components/atoms/img.tsx
+++ src/components/atoms/img.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import Image, { type ImageProps, type StaticImageData } from 'next/image';
+import { useState } from 'react';
 
 import cx from '@/utils/cx';
 
@@ -25,6 +28,7 @@ const getProps = (baseProps: ImgProps) => {
 };
 
 export const Img = (baseProps: ImgProps) => {
+  const [errored, setErrored] = useState<boolean>(false);
   const { width = 0, height = 0, ...props } = getProps(baseProps);
   return (
     // Disabled warning. Alt props already is present in props
@@ -43,6 +47,10 @@ export const Img = (baseProps: ImgProps) => {
       className={cx('object-cover object-center', props.className)}
       loading={!props.priority ? 'lazy' : undefined}
       decoding={'async'}
+      onError={() => {
+        setErrored(true);
+      }}
+      unoptimized={errored || props.unoptimized}
     />
   );
 };
