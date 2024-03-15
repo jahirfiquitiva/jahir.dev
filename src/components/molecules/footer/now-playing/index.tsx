25487e4810f20d558c6d33398fb5632b3088d89c
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2024-03-15 00:15:36 -0500

    chore: revert dynamic import of footer now playing component

--- src/components/molecules/footer/now-playing/index.tsx
+++ src/components/molecules/footer/now-playing/index.tsx
@@ -1,13 +1,56 @@
 'use client';
 
-import dynamic from 'next/dynamic';
-
 import { Icon } from '@/components/atoms/icon';
-import { loading } from '@/components/icons';
+import { loading as loadingIcon } from '@/components/icons';
+import { useRequest } from '@/hooks/use-request';
+import type { NowPlayingAPIResponse } from '@/types/spotify/request';
 
-const DynamicFooterNowPlaying = dynamic(() => import('./now-playing'), {
-  ssr: false,
-  loading: () => <Icon path={loading} className={'size-5 animate-spin'} />,
-});
+import {
+  NowPlayingAlbumCover,
+  NowPlayingLink,
+  NowPlayingTextsContainer,
+  ScrollingText,
+} from './now-playing.styles';
+import { Clock } from './time';
 
-export const FooterNowPlaying = () => <DynamicFooterNowPlaying />;
+export const FooterNowPlaying = () => {
+  const { data, loading } =
+    useRequest<NowPlayingAPIResponse>('/api/now-playing');
+  const { track, isPlaying } = data || { isPlaying: false };
+
+  if (loading)
+    return <Icon path={loadingIcon} className={'size-5 animate-spin'} />;
+
+  if (!isPlaying || !track) return <Clock />;
+  const scrollingText = `${track.name} by ${track.artist}`;
+  const animationDuration = scrollingText.length * 0.35;
+  return (
+    <NowPlayingLink
+      title={`Listen to "${track.name}" by "${track.artist}" on Spotify`}
+      href={track.url}
+      target={'_blank'}
+      data-umami-event={'Now Playing'}
+      data-umami-event-from={'Footer'}
+      style={{ maxWidth: '28ch' }}
+    >
+      <NowPlayingAlbumCover
+        alt={`Album cover: "${track.album}" by "${track.artist}"`}
+        src={track.image?.url || ''}
+        width={24}
+        height={24}
+        quality={50}
+      />
+      <NowPlayingTextsContainer
+        style={{ animationDuration: `${animationDuration}s` }}
+      >
+        <ScrollingText>{scrollingText}</ScrollingText>
+        <ScrollingText aria-hidden={'true'} className={'select-none'}>
+          {scrollingText}
+        </ScrollingText>
+        <ScrollingText aria-hidden={'true'} className={'select-none'}>
+          {scrollingText}
+        </ScrollingText>
+      </NowPlayingTextsContainer>
+    </NowPlayingLink>
+  );
+};
